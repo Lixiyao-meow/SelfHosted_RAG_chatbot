5bbeb47fe4730fd10c6a713d355def69cd020376
lixiyao-meow <lixiyao0218@hotmail.com>
2023-10-18 15:17:26 +0200

    change markdown loader to text loader

--- src/markdown_loader.py
+++ src/markdown_loader.py
@@ -1,17 +1,25 @@
-import glob
 import os
+import fnmatch
 
-from langchain.document_loaders import UnstructuredMarkdownLoader
+from langchain.document_loaders import TextLoader
 
 class MarkdownLoader():
 
     def __init__(self, markdown_path):
         self.markdown_path = markdown_path
 
+    # find all .md files recursively and save the path
     def load_batch(self):
         docs = []
-        for md_file in glob.glob(os.path.join(self.markdown_path, '*.md')):
-            loader = UnstructuredMarkdownLoader(md_file)
-            data = loader.load()
-            docs.append(data[0])
+        for dirpath, dirs, files in os.walk(self.markdown_path): 
+            for filename in fnmatch.filter(files, '*.md'):
+                md_file = os.path.join(dirpath, filename)
+                loader = TextLoader(md_file, encoding="utf-8")
+                data = loader.load()
+
+                # add relative location and filename of data to metadata
+                data[0].metadata["dirpath"] = dirpath
+                data[0].metadata["filename"] = filename
+                docs.append(data[0])
+
         return docs
